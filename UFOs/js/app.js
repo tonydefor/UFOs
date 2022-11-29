7fb7c7f5daaefa75ed707cde831662a9a9297830
tonydefor <47859209+tonydefor@users.noreply.github.com>
2022-11-28 21:44:24 -0500

    Add files via upload

--- UFOs/js/app.js
+++ UFOs/js/app.js
@@ -24,23 +24,40 @@ function buildTable(data) {
 }
 
 // 1. Create a variable to keep track of all the filters as an object.
-
+filterData = {
+// datetime: "", 
+// city: "",
+// state: "",
+// country: "",
+// shape: "",
+}
 
 // 3. Use this function to update the filters. 
-function updateFilters() {
-
+function updateFilters(event) {
+  console.log('hello')
     // 4a. Save the element that was changed as a variable.
-
+let value = d3.select(this).property("value")
+  console.log(value)
     // 4b. Save the value that was changed as a variable.
-
+let element = d3.select(this).property("id")
+    console.log(element)
     // 4c. Save the id of the filter that was changed as a variable.
 
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
- 
+    // if statement to check if value is empty
+ filterData[element] = value
+  console.log(filterData)
   
     // 6. Call function to apply all filters and rebuild the table
+    if(value) {
+      filterData[element] = value
+    }
+    else {
+      delete filterData[element]
+    }
+
     filterTable();
   
   }
@@ -49,18 +66,20 @@ function updateFilters() {
   function filterTable() {
   
     // 8. Set the filtered data to the tableData.
-    
+    let filterArr = tableData
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    
+    Object.entries(filterData).forEach(([key, value])=> {
+      filterArr = filterArr.filter(row => row[key]===value)
+    })
   
     // 10. Finally, rebuild the table using the filtered data
-    
+    buildTable(filterArr);
   }
   
   // 2. Attach an event to listen for changes to each filter
-  
+  d3.selectAll(".filterdata").on("change", updateFilters)
   
   // Build the table when the page loads
   buildTable(tableData);
